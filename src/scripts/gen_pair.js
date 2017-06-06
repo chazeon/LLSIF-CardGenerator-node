2e9df9dfd36db0dfd636d8eb45475d606c256d33
Chazeon <chenxing.luo@gmail.com>
2017-06-06 22:09:17 +0800

    Updating `gen_pair.js` to keep it up with latest API

--- src/scripts/gen_pair.js
+++ src/scripts/gen_pair.js
@@ -25,7 +25,7 @@ unitFactory.createAllPairs()
     return new Promise((resolve, reject) => {
         let filename = path.join(config.dir.generated, `frameless/pairs/${pair.pairId}.png`)
         if (!fs.existsSync(filename))
-            viewFactory.createPairView(pair, { framed: false }).toFile(filename)
+            viewFactory.createPairView(pair, { mode: 'frameless' }).toFile(filename)
             .then(() => resolve(pair))
             .catch(err => reject(err))
         else
